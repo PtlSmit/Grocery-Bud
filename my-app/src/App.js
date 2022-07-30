1e1ffce77d63be62e60d1f5f3f23c6db0003de29
Smit <ptlsmit.b@gmail.com>
2022-07-29 17:29:36 -0700

    Initial Setup done

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import List from './List'
+import Alert from './Alert'
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+
+export default App
